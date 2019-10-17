e394046976a35828019c88e2bbc17ce8ddaf8645
azouhir <1835227@brunel.ac.uk>
2019-10-17 15:16:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.diceroller;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+int count;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+            public void on_button_click(View view) {
+
+                TextView result = null;
+                try {
+                    Random ran = new Random();
+                    int rando = ran.nextInt(6 + 1 - 1) + 1;
+
+                    result = this.findViewById(R.id.textView);
+                    EditText input = this.findViewById(R.id.editText);
+                    String UserInput = input.getText().toString();
+                    int guess = Integer.parseInt(UserInput);
+
+                    TextView score = this.findViewById(R.id.textView2);
+                    score.setText("Your Score is: " + Integer.toString(count));
+                    result.setText(Integer.toString(rando));
+
+                    if (guess == rando) {
+                        count++;
+                    }
+                } catch (Exception e) {
+                    result.setText("Please enter a number as your guess");
+                }
+            }
+
+
+    }
+
